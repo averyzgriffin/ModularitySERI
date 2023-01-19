6862ab50e7a427ea5adb80f179696fb6106ee33e
Avery Griffin <averyzanegriffin@gmail.com>
2023-01-18 16:50:45 -0800

    Split up the load and evaluate functions in post processing

--- post_processing.py
+++ post_processing.py
@@ -3,26 +3,31 @@ from matplotlib import pyplot as plt
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+
+from analysis import plot_valid_losses
+from eigen import compute_eigens
+from gram import compute_grams, preprocess_lams
 from models import OrthogMLP
 
 
-def load_and_evaluate_models(model_dir, loss_func, device, N, batch_size):
-    test_loader = torch.utils.data.DataLoader(datasets.MNIST('../mnist_data', download=True, train=False,
-                                                             transform=transforms.Compose([transforms.ToTensor(),
-                                                                       transforms.Normalize((0.1307,), (0.3081,))])),
-                                                            batch_size=batch_size,
-                                                            shuffle=True)
-
+def load_models(model_dir, device, N):
     model_paths = [os.path.join(model_dir, path) for path in os.listdir(model_dir) if path.endswith('.pt')]
     sorted_paths = sorted(model_paths, key=lambda x: int(x.split("_")[-1].split(".")[0]))
-    scores = []
+    models = []
     for path in sorted_paths:
         model = OrthogMLP(784, *N).to(device)
         model.load_state_dict(torch.load(path))
-        score = validation_fn(model, test_loader, loss_func, device)
+        models.append(model)
+    return models
+
+
+def evaluate_models(models, loss_func, test_loader, device):
+    scores = []
+    for m in range(len(models)):
+        score = validation_fn(models[m], test_loader, loss_func, device)
         scores.append(score)
-        print(f'Validation score for {path} is {score}')
-    # Plot the losses
+        print(f'Validation score for Model {m} is {score}')
+
     plt.plot(scores)
     plt.xlabel('Epoch')
     plt.ylabel('Validation Accuracy')
