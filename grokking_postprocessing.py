bb5b39931f99d83c76920199d2f06630b27eeef0
Avery Griffin <averyzanegriffin@gmail.com>
2023-02-08 10:33:53 -0800

    Removed unused parts

--- grokking_postprocessing.py
+++ grokking_postprocessing.py
@@ -9,23 +9,6 @@ from analysis import interactive_histogram
 
 
 def main(models: list, data, labels, which_models):
-    num_heads = 4
-
-    # W_O = einops.rearrange(model.blocks[0].attn.W_O, 'm (i h)->i m h', i=num_heads)
-    # W_K = model.blocks[0].attn.W_K
-    # W_Q = model.blocks[0].attn.W_Q
-    # W_V = model.blocks[0].attn.W_V
-    # W_in = model.blocks[0].mlp.W_in
-    # W_out = model.blocks[0].mlp.W_out
-    # W_pos = model.pos_embed.W_pos.T
-    # # We remove the equals sign dimension from the Embed and Unembed, so we can
-    # # apply a Fourier Transform over R^p
-    # W_E = model.embed.W_E[:, :-1]
-    # W_U = model.unembed.W_U[:, :-1].T
-    #
-    # # The initial value of the residual stream at position 2 - constant for all inputs
-    # final_pos_resid_initial = model.embed.W_E[:, -1] + W_pos[:, 2]
-
     scores = []
     eigs = []
     num = 1
@@ -38,20 +21,12 @@ def main(models: list, data, labels, which_models):
         model.cache_all(cache)
 
         # Forward Pass
-        # data = data[:5]
-        # labels = labels[:5]
-
         original_logits = model(data)[:, -1]
         original_logits = original_logits[:, :-1]
 
         # Get loss
         original_loss = cross_entropy_high_precision(original_logits, labels)
         score = original_loss.to("cpu").detach().float() # TODO + something
-
-        # Check out particular hooks
-        # attn_mat = cache['blocks.0.attn.hook_attn'][:, :, 2, :2]
-        # neuron_acts = cache['blocks.0.mlp.hook_post'][:, -1]
-        # neuron_acts_pre = cache['blocks.0.mlp.hook_pre'][:, -1]
 
         def compute_grams(cache):
             grams = {}
@@ -61,13 +36,6 @@ def main(models: list, data, labels, which_models):
             act_dict["blocks.0.attn.hook_z"] = torch.cat((act_dict["blocks.0.attn.hook_z"], act_dict["blocks.0.hook_resid_pre"]), dim=2)
             act_dict["blocks.0.mlp.hook_post_hidden"] = torch.cat((act_dict["blocks.0.mlp.hook_post_hidden"], act_dict["blocks.0.hook_resid_mid"]), dim=2)
             for name, activation in act_dict.items():
-                # activation = activation[:,0,:]
-                # activation = torch.tensor([
-                #     [[1,2], [3,4], [5,6]],
-                #     [[7,8], [9,10], [11,12]],
-                #     [[13,14], [15,16], [17,18]],
-                #     [[19,20], [21,22], [23,24]],
-                # ])
                 chunks = torch.split(activation, 100, dim=0)
                 for chunk in chunks:
                     if name in grams:
@@ -90,8 +58,9 @@ def main(models: list, data, labels, which_models):
 
         eigenvalues, eigenvectors = compute_eigens(grams)
 
-        # Preprocess Eigens  TODO not sure how to handle this part yet
+        # Preprocess Eigens - # This isn't necessary but technically is in the math
         # repeated_tensors = [lams[i].repeat(N[i]) for i in range(len(lams))]
+
         all_eigenvalues = torch.cat([lam.to("cpu").detach() for lam in eigenvalues.values()], dim=0).to("cpu").detach()
 
         scores.append(score)
