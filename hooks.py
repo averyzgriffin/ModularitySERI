5b8431ec3ee53c2ad17d471695427e641d5ae091
Avery Griffin <averyzanegriffin@gmail.com>
2023-02-10 17:25:32 -0800

    Just commented out all but first three layers

--- hooks.py
+++ hooks.py
@@ -2,8 +2,8 @@ import torch
 
 
 def grab_activations(cache):
-    which_layers = ["blocks.0.hook_resid_pre", 'blocks.0.attn.hook_vprime_concat', 'blocks.0.hook_resid_mid',
-                    'blocks.0.mlp.hook_hidden', 'blocks.0.hook_resid_post', 'hook_unembed_post']
+    which_layers = ["blocks.0.hook_resid_pre", 'blocks.0.attn.hook_vprime_concat', 'blocks.0.hook_resid_mid']#
+                    #,'blocks.0.mlp.hook_hidden', 'blocks.0.hook_resid_post', 'hook_unembed_post']
 
     activations = {k: v for (k, v) in cache.items() if k in which_layers}
     # activations["blocks.0.post_attn.hook_z"] = torch.cat(
