3f83d218dfa60e08387281fb0cd7f18e87e83d9b
Avery Griffin <averyzanegriffin@gmail.com>
2023-02-22 12:02:09 -0800

    Updated hooks for computing edges

--- hooks.py
+++ hooks.py
@@ -2,14 +2,18 @@ import torch
 
 
 def grab_activations(cache):
-    which_layers = ["blocks.0.hook_resid_pre", 'blocks.0.attn.hook_vprime_concat', 'blocks.0.hook_resid_mid']#
-                    #,'blocks.0.mlp.hook_hidden', 'blocks.0.hook_resid_post', 'hook_unembed_post']
+    which_layers = ["embed.hook_input", "blocks.0.hook_resid_pre", 'blocks.0.attn.hook_vprime_concat', 'blocks.0.hook_resid_mid'
+                    ,'blocks.0.mlp.hook_hidden', 'blocks.0.hook_resid_post', 'hook_unembed_post']
 
     activations = {k: v for (k, v) in cache.items() if k in which_layers}
-    # activations["blocks.0.post_attn.hook_z"] = torch.cat(
-    #     (activations["blocks.0.post_attn.hook_z"], activations["blocks.0.hook_resid_pre"]), dim=2)
-    # activations["blocks.0.mlp.hook_post_hidden"] = torch.cat(
-    #     (activations["blocks.0.mlp.hook_post_hidden"], activations["blocks.0.hook_resid_mid"]), dim=2)
+
+    batch_size = len(activations['blocks.0.hook_resid_mid'])
+    seq_len = activations['blocks.0.hook_resid_mid'].shape[1]
+
+    # Add bias to input, R2, and Hidden layer
+    activations["embed.hook_input"] = torch.concat((torch.ones((batch_size,seq_len,1)).to("cuda:0"), activations['embed.hook_input']), dim=2)
+    activations["blocks.0.hook_resid_mid"] = torch.concat((torch.ones((batch_size,seq_len,1)).to("cuda:0"), activations['blocks.0.hook_resid_mid']), dim=2)
+    activations["blocks.0.mlp.hook_hidden"] = torch.concat((torch.ones((batch_size,seq_len,1)).to("cuda:0"), activations['blocks.0.mlp.hook_hidden']), dim=2)
 
     return activations
 
