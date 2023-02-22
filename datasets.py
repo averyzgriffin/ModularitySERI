feb627ee7aecb8106173781d4a9eeedbe800b03f
Avery Griffin <averyzanegriffin@gmail.com>
2023-02-22 12:03:26 -0800

    Updated the modular arithmetic dataset class to have a train/test split

--- datasets.py
+++ datasets.py
@@ -1,6 +1,6 @@
 import itertools
-
 import numpy as np
+import random
 import torch
 from torch.utils.data import Dataset
 
@@ -9,26 +9,46 @@ class ModularArithmeticDataset(torch.utils.data.Dataset):
     """
     Dataset used in Neel Nanda's modular arithmetic work.
     """
-    def __init__(self, p, fn_name, device):
+    def __init__(self, p, fn_name, device, split=0.8, seed=0, train=True):
         self.p = p
         self.fn_name = fn_name
         self.device = device
+        self.split = split
+        self.seed = seed
+        self.train = train
 
         self.fns_dict = {'add': lambda x, y: (x + y) % self.p, 'subtract': lambda x, y: (x - y) % self.p,
                          'x2xyy2': lambda x, y: (x ** 2 + x * y + y ** 2) % self.p}
         self.fn = self.fns_dict[fn_name]
         self.x, self.labels = self.construct_dataset()
+        self.train_x, self.train_labels, self.test_x, self.test_labels = self.split_dataset()
 
     def __getitem__(self, index):
-        return self.x[index], self.labels[index]
+        if self.train:
+            return self.train_x[index], self.train_labels[index]
+        else:
+            return self.test_x[index], self.test_labels[index]
 
     def __len__(self):
-        return len(self.x)
+        if self.train:
+            return len(self.train_x)
+        else:
+            return len(self.test_x)
 
     def construct_dataset(self):
         x = torch.tensor([(i, j, self.p) for i in range(self.p) for j in range(self.p)]).to(self.device)
         y = torch.tensor([self.fn(i, j) for i, j, _ in x]).to(self.device)
         return x, y
+
+    def split_dataset(self):
+        random.seed(self.seed)
+        indices = list(range(len(self.x)))
+        random.shuffle(indices)
+        div = int(self.split * len(indices))
+        train_indices, test_indices = indices[:div], indices[div:]
+        train_x, train_labels = self.x[train_indices], self.labels[train_indices]
+        test_x, test_labels = self.x[test_indices], self.labels[test_indices]
+        return train_x, train_labels, test_x, test_labels
 
 
 class RetinaDataset(Dataset):
